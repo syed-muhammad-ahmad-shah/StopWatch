3e542ef3d6d658c97d900f9001e80e0ee8fb0e5a
Ahmad Shah <101276981+SyedMuhammadAhmadShah123@users.noreply.github.com>
2022-12-04 18:10:10 +0500

    commit

--- script.js
+++ script.js
@@ -2,28 +2,19 @@ let h = 0;
 let m = 0;
 let s = 0;
 let ms = 0;
-let st ;
-let count
+let str ;
+let count;
 
-function start(){
-    if(!st){
+// this startHtml is for calling event of click on start button
+const startHtml = document.getElementById("start");
+// this stopHtml is for calling event of click on stop button
+const stopHtml = document.getElementById("stop");
+// this resetHtml is for calling event of click on reset button
+const resetHtml = document.getElementById("reset");
+// this lapHtml is for calling event of click on lap button
+const lapHtml = document.getElementById("lap");
 
-        st = setInterval(timer,10)
-        count = 1
-    }
-}
-
-
-// ======================== start obj =================================
-// function startObj(start) {
-//     this.st 
-//     if(!st){
-
-//         st = setInterval(timer,10)
-//         count = 1
-//     }
-//  }
- 
+// ======================== timer function ==========================
 function timer(){
     let display = document.getElementById("timer")
     display.innerHTML=(h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
@@ -42,7 +33,141 @@ function timer(){
     }
 } 
 
-// ======================== timer obj =================================
+// ======================== start obj =================================
+let startObject = {
+    startMethod : function (startPara) {
+                st = startPara 
+                if(!st) {
+                    st = setInterval(timer,10)
+                    count = 1
+                    return st
+                }
+            }
+}
+// this startHtml is declared above
+startHtml.addEventListener("click", function() {
+     startObject.startMethod(str);
+    });
+
+
+// ======================== stop obj =================================
+let stopObject = {  
+    stopMethod : function () {
+        clearInterval(st);
+        st = false;
+    }
+}
+// this stopHtml is declared above
+stopHtml.addEventListener("click", function() {
+    stopObject.stopMethod();
+   });
+
+// ======================== reset obj =================================
+let resetObject = {  
+    resetMethod : function () {
+        h = 0;
+        m = 0;
+        s = 0;
+        ms = 0;
+        count = 0;
+        // document.getElementById("timer").innerHTML = (h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
+        document.getElementById("timer").innerHTML = "00:00:00:00"
+        stopObject.stopMethod()
+    }
+}
+// this resetHtml is declared above
+resetHtml.addEventListener("click", function() {
+    resetObject.resetMethod();
+   });
+   
+// ======================== lap obj =================================
+let lapObject = {  
+    lapMethod : function () {
+        if(count == 1){
+            let li = document.createElement("li");
+            li.setAttribute("class","lap")
+            li.innerHTML = (h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
+            let ul = document.getElementById("laps");
+            ul.appendChild(li);
+        }
+    }
+}
+// this lapHtml is declared above
+lapHtml.addEventListener("click", function() {
+    lapObject.lapMethod();
+   });
+
+/* ================== 
+code below is just for Reference
+======================*/
+
+// ========================start function=========================
+// function start(){
+//     if(!st){
+
+//         st = setInterval(timer,10)
+//         count = 1
+//     }
+// }
+
+// ======================== stop function ==========================
+// function stop(){
+//     clearInterval(st);
+   
+//     st = false;
+// }
+
+// ======================== reset function ==========================
+// function reset(){
+//     h = 0;
+//     m = 0;
+//     s = 0;
+//     ms = 0;
+//     count = 0;
+//     // document.getElementById("timer").innerHTML = (h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
+//     document.getElementById("timer").innerHTML = "00:00:00:00"
+//     stop()
+// }
+
+// ======================== reset function ==========================
+// function lap(){
+//     if(count == 1){
+//         let li = document.createElement("li");
+//         li.setAttribute("class","lap")
+//         li.innerHTML = (h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
+//         let ul = document.getElementById("laps");
+//         ul.appendChild(li);
+//     }
+// }
+
+// ======================== start constructor obj =================================
+// function startObj(start) {
+//     this.startAttribute = start
+//     this.startMethod = function () {
+//         console.log("lkjlkj")
+//         if(!st) {
+//             st = setInterval(timer,10)
+//             count = 1
+//             console.log("lkjlkj")
+//         }
+//     }
+// }
+// let startObject = new startObj(str)
+// console.log("kljl",startObject.start)
+// start.addEventListener("click",function(){
+//      startObject.startMethod;
+//     //  console.log(startObj.start)
+//     });
+
+// ======================== stop constructor obj =================================
+// function stopObj() {
+//     clearInterval(this.st)
+//     this.st = false
+//     startObj(this.st)
+
+// }
+
+// ======================== timer constructor obj =================================
 // function timerObj(){
 //     this.display = document.getElementById("timer")
 //     display.innerHTML=(h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
@@ -60,44 +185,6 @@ function timer(){
 //         h++
 //     }
 // } 
-
-
-function stop(){
-    clearInterval(st);
-   
-    st = false;
-}
-
-// ======================== stop obj =================================
-// function stopObj() {
-//     clearInterval(this.st)
-//     this.st = false
-//     startObj(this.st)
-
-// }
-
-function reset(){
-    h = 0;
-    m = 0;
-    s = 0;
-    ms = 0;
-    count = 0;
-    // document.getElementById("timer").innerHTML = (h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
-    document.getElementById("timer").innerHTML = "00:00:00:00"
-    stop()
-}
-
-function lap(){
-    if(count == 1){
-        let li = document.createElement("li");
-        li.setAttribute("class","lap")
-        li.innerHTML = (h < 10?"0"+h:h)+":"+(m < 10?"0"+m:m)+":"+(s < 10?"0"+s:s)+":"+(ms < 10?"0"+ms:ms);
-        let ul = document.getElementById("laps");
-        ul.appendChild(li);
-    }
-}
-
-
 
 
 
